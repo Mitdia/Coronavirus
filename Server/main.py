4ac965e8e1541c9fd6e406cde3bf2c7ce19f06a2
Вострых Михаил Анатольевич <MAVostrykh@domclick.ru>
2021-03-28 22:51:46 +0300

    add Dockerfile

--- Server/main.py
+++ Server/main.py
@@ -1,29 +1,38 @@
+from math import log2, pi, sqrt
+from pathlib import Path
+
 import pandas as pd
+from bokeh.embed import file_html
+from bokeh.io import curdoc, show
+from bokeh.layouts import column, row
+from bokeh.models import Button, CustomJS, Dropdown, Paragraph, Select
+from bokeh.models.tools import *
+from bokeh.plotting import figure, output_file
+from bokeh.server.server import Server
+from bokeh.settings import settings
+
 import map_data
 
-from bokeh.plotting import figure, output_file
-from bokeh.io import show, curdoc
-from bokeh.embed import file_html
-from bokeh.models import CustomJS, Select, Dropdown, Button, Paragraph
-from bokeh.models.tools import *
-from bokeh.layouts import column, row
-from bokeh.settings import settings
-from math import sqrt, log2, pi
-
-
+PLT_WIDTH = 1500
+PLT_HEIGHT = 866
 settings.resources = "inline"
 settings.log_level = "debug"
 
-def parser():
+
+def init():
     global data, regions, mutations_names, information_about_mutations
-    file_for_data = open("Data/data.csv", 'r')
-    file_for_regions = open("Data/regions.txt", 'r')
-    file_for_mutations_names = open("Data/mutations_names.txt", 'r')
-    file_for_information_about_mutations = open("Data/information_about_mutations.txt", 'r')
+    file_for_data = open(Path("Data", "data.csv"), "r")
+    file_for_regions = open(Path("Data", "regions.txt"), "r")
+    file_for_mutations_names = open(Path("Data", "mutations_names.txt"), "r")
+    file_for_information_about_mutations = open(
+        Path("Data", "information_about_mutations.txt"), "r"
+    )
     data = pd.read_csv(file_for_data, header=0)
-    regions = file_for_regions.read().split(',')
-    mutations_names = file_for_mutations_names.read().split(',')
-    information_about_mutations_raw = file_for_information_about_mutations.read().split('\n')
+    regions = file_for_regions.read().split(",")
+    mutations_names = file_for_mutations_names.read().split(",")
+    information_about_mutations_raw = file_for_information_about_mutations.read().split(
+        "\n"
+    )
     information_about_mutations = {}
     for mutation_index in range(0, len(information_about_mutations_raw) - 1, 2):
         mutation_name = information_about_mutations_raw[mutation_index]
@@ -31,17 +40,7 @@ def parser():
         information_about_mutations[mutation_name] = information_about_mutation
 
 
-def get_mutations_names():
-    mutations_data = open("Data\\RUSSIA.INTERESTING_VARIANTS.COUCCURRENCE.JAN.txt", 'r')
-    data_about_mutations = pd.read_csv(mutations_data, sep='\t', header=0)
-    parsed_mutation_names = []
-    for pd_column in data_about_mutations.columns[1:]:
-        parsed_mutation_names.append(pd_column)
-    return parsed_mutation_names
-
-
 def configure_plot():
-    global plt_width, plt_height
     zoom = WheelZoomTool(zoom_on_axis=False)
     reset = ResetTool()
     hover = HoverTool()
@@ -52,16 +51,29 @@ def configure_plot():
         </div>
     """
     tools = [zoom, reset, hover, pan]
-    p = figure(plot_width=plt_width, plot_height=plt_height, tooltips=TOOLTIPS,
-    tools=tools, toolbar_location="right", margin=[0, 100, 0, 100])
+    p = figure(
+        plot_width=PLT_WIDTH,
+        plot_height=PLT_HEIGHT,
+        tooltips=TOOLTIPS,
+        tools=tools,
+        toolbar_location="right",
+        margin=[0, 100, 0, 100],
+    )
     p.grid.visible = False
     p.axis.visible = False
     p.outline_line_color = "green"
     p.match_aspect = True
     p.toolbar.active_scroll = zoom
-    p.image_url(url=['https://ma.fbb.msu.ru/coronamap/static/map.svg'], x=0, y=0,
-    w=plt_width, h=plt_height, anchor="bottom_left")
+    p.image_url(
+        url=["https://ma.fbb.msu.ru/coronamap/static/map.svg"],
+        x=0,
+        y=0,
+        w=PLT_WIDTH,
+        h=PLT_HEIGHT,
+        anchor="bottom_left",
+    )
     return p
+
 
 def create_main_map():
     p = configure_plot()
@@ -69,9 +81,14 @@ def create_main_map():
         coordinates_of_region = map_data.coordinates[region]
         number_of_samples = (data.location == region).sum()
         radius = log2(number_of_samples + 1) * 5
-        p.circle(x=coordinates_of_region[0], y=plt_height - coordinates_of_region[1], radius=radius, alpha=0.5,
-                     fill_color="blue",
-                     name=f"{region}: {number_of_samples}")
+        p.circle(
+            x=coordinates_of_region[0],
+            y=PLT_HEIGHT - coordinates_of_region[1],
+            radius=radius,
+            alpha=0.5,
+            fill_color="blue",
+            name=f"{region}: {number_of_samples}",
+        )
     return p
 
 
@@ -79,42 +96,77 @@ def create_map(mutation_name, data):
     p = configure_plot()
     for region in regions:
         coordinates_of_region = map_data.coordinates[region]
-        region_data = data.loc[data['location'] == region]
+        region_data = data.loc[data["location"] == region]
         all_variants = len(region_data)
         if all_variants == 0:
-            p.circle(x=coordinates_of_region[0], y=plt_height - coordinates_of_region[1], radius=10, alpha=0.1,
-                     fill_color="blue",
-                     name=f"{region}: данные отсутсвуют")
+            p.circle(
+                x=coordinates_of_region[0],
+                y=PLT_HEIGHT - coordinates_of_region[1],
+                radius=10,
+                alpha=0.1,
+                fill_color="blue",
+                name=f"{region}: данные отсутсвуют",
+            )
             continue
         mutated_variants = region_data[mutation_name].sum()
         nonmutated_variants = all_variants - mutated_variants
         if nonmutated_variants == 0:
-            p.circle(x=coordinates_of_region[0], y=plt_height - coordinates_of_region[1], radius=10, alpha=0.8,
-                     fill_color="red",
-                     name=f"{region}: {int(mutated_variants)}/{all_variants}")
+            p.circle(
+                x=coordinates_of_region[0],
+                y=PLT_HEIGHT - coordinates_of_region[1],
+                radius=10,
+                alpha=0.8,
+                fill_color="red",
+                name=f"{region}: {int(mutated_variants)}/{all_variants}",
+            )
         elif mutated_variants == 0:
-            p.circle(x=coordinates_of_region[0], y=plt_height - coordinates_of_region[1], radius=10, alpha=0.8,
-                     fill_color="green",
-                     name=f"{region}: {int(mutated_variants)}/{all_variants}")
+            p.circle(
+                x=coordinates_of_region[0],
+                y=PLT_HEIGHT - coordinates_of_region[1],
+                radius=10,
+                alpha=0.8,
+                fill_color="green",
+                name=f"{region}: {int(mutated_variants)}/{all_variants}",
+            )
         else:
             first_angle = mutated_variants / all_variants * 2 * pi
             second_angle = nonmutated_variants / all_variants * 2 * pi
-            first_color = 'red'
-            second_color = 'green'
-            first_name = f'{region}: {int(mutated_variants)}/{all_variants}'
-            p.wedge(x=coordinates_of_region[0], y=plt_height - coordinates_of_region[1], radius=10,
-                    start_angle=0, fill_color=first_color, end_angle=first_angle, line_color="white", name=first_name,
-                    alpha=0.8)
-            p.wedge(x=coordinates_of_region[0], y=plt_height - coordinates_of_region[1], radius=10,
-                    start_angle=first_angle, fill_color=second_color, end_angle=(second_angle + first_angle),
-                    line_color="white", name=first_name, alpha=0.8)
+            first_color = "red"
+            second_color = "green"
+            first_name = f"{region}: {int(mutated_variants)}/{all_variants}"
+            p.wedge(
+                x=coordinates_of_region[0],
+                y=PLT_HEIGHT - coordinates_of_region[1],
+                radius=10,
+                start_angle=0,
+                fill_color=first_color,
+                end_angle=first_angle,
+                line_color="white",
+                name=first_name,
+                alpha=0.8,
+            )
+            p.wedge(
+                x=coordinates_of_region[0],
+                y=PLT_HEIGHT - coordinates_of_region[1],
+                radius=10,
+                start_angle=first_angle,
+                fill_color=second_color,
+                end_angle=(second_angle + first_angle),
+                line_color="white",
+                name=first_name,
+                alpha=0.8,
+            )
     return p
 
 
 def render_text(mutation_name):
-    global information_about_mutations, plt_width, plt_height
     information_about_mutation = information_about_mutations[mutation_name]
-    text = Paragraph(text=information_about_mutation, width=(plt_width//2), height=plt_height,  margin=[0, 100, 0, 100])
+    text = Paragraph(
+        text=information_about_mutation,
+        width=(PLT_WIDTH // 2),
+        height=PLT_HEIGHT,
+        margin=[0, 100, 0, 100],
+    )
     return text
 
 
@@ -139,19 +191,54 @@ def show_main_map():
     doc.add_root(last_module)
 
 
-plt_width = 1500
-plt_height = 866
-parser()
+def run(doc):
+    select = Select(
+        min_height=50,
+        title="Mutation",
+        value=mutations_names[0],
+        options=mutations_names,
+        margin=[0, 0, 0, 100],
+    )
+    button = Button(
+        min_height=50,
+        label="Number of samples per regions",
+        button_type="success",
+        margin=[0, 0, 0, 100],
+    )
+    select.on_change("value", update_plot)
+    button.on_click(show_main_map)
+    controls = column(button, select)
+    last_module = column(create_main_map(), sizing_mode="scale_width")
+    doc = curdoc()
 
-select = Select(min_height=50, title="Mutation", value=mutations_names[0], options=mutations_names, margin=[0, 0, 0, 100])
-button = Button(min_height=50, label="Number of samples per regions", button_type="success", margin=[0, 0, 0, 100])
-select.on_change('value', update_plot)
-button.on_click(show_main_map)
-controls = column(button, select)
-last_module = column(create_main_map(), sizing_mode="scale_width")
-doc = curdoc()
+    doc.title = "Coronavirus"
+    doc.add_root(controls)
+    doc.add_root(last_module)
 
 
-doc.title = 'Coronavirus'
-doc.add_root(controls)
-doc.add_root(last_module)
+def test(doc):
+    x = [1, 2, 3, 4, 5]
+    y = [6, 7, 2, 4, 5]
+
+    fig = figure(title="simple line example", x_axis_label="x", y_axis_label="y")
+    fig.line(x, y, legend="Temp.", line_width=2)
+
+    layout = column(fig)
+    doc.add_root(layout)
+
+
+if __name__ == "__main__":
+    init()
+    print("init complete")
+    kws = {
+        "address": "0.0.0.0",
+        "port": 5006,
+        "prefix": "/bokeh",
+        "allow_websocket_origin": ["*"],
+    }
+    server = Server(test, **kws)
+    # server = Server(run, **kws)
+    server.start()
+    if __name__ == "__main__":
+        server.io_loop.add_callback(server.show, "/")
+        server.io_loop.start()
--- data_parser.py
+++ data_parser.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pandas as pd
 
 
@@ -13,27 +15,31 @@ def translate_regions_names_from_russian_to_english(region_name):
             return regions_names[0]
 
 
-def parse_data(adress="Data\\RussianSamples.Mutations.UpTo28thFeb.txt"):
-    main_data = open(adress, 'r')
-    parsed_data = pd.read_csv(main_data, sep='\t', header=None)
+def parse_data(adress=Path("Data", "RussianSamples.Mutations.UpTo28thFeb.txt")):
+    main_data = open(adress, "r")
+    parsed_data = pd.read_csv(main_data, sep="\t", header=None)
 
     mutations_names = []
     for mutations in parsed_data[3]:
-        for mutation_name in mutations.split(','):
-            if mutation_name not in mutations_names and mutation_name != '':
+        for mutation_name in mutations.split(","):
+            if mutation_name not in mutations_names and mutation_name != "":
                 mutations_names.append(mutation_name)
     mutations_names.sort()
-    parsed_data = parsed_data.rename(columns={0: 'sample name', 1: 'date', 2: 'location', 3: 'mutations'})
+    parsed_data = parsed_data.rename(
+        columns={0: "sample name", 1: "date", 2: "location", 3: "mutations"}
+    )
 
-    for region_index in range(len(parsed_data['location'])):
-        region = translate_regions_names_from_english_to_russian(parsed_data['location'][region_index])
-        parsed_data['location'][region_index] = region
+    for region_index in range(len(parsed_data["location"])):
+        region = translate_regions_names_from_english_to_russian(
+            parsed_data["location"][region_index]
+        )
+        parsed_data["location"][region_index] = region
 
     for mutation_index in range(len(mutations_names)):
         mutation_parsed = []
         mutation = mutations_names[mutation_index]
-        for sample_mutations in parsed_data['mutations']:
-            if mutation in sample_mutations.split(','):
+        for sample_mutations in parsed_data["mutations"]:
+            if mutation in sample_mutations.split(","):
                 mutation_parsed.append(1)
             else:
                 mutation_parsed.append(0)
@@ -42,11 +48,11 @@ def parse_data(adress="Data\\RussianSamples.Mutations.UpTo28thFeb.txt"):
     return parsed_data, mutations_names
 
 
-def parse_regions(adress='Data\\RegionsInGADM.txt') :
-    regions_dict = open(adress, 'r')
-    regions_dictionary = regions_dict.read().split('\n')
+def parse_regions(adress=Path("Data", "RegionsInGADM.txt")):
+    regions_dict = open(adress, "r")
+    regions_dictionary = regions_dict.read().split("\n")
     for index in range(len(regions_dictionary)):
-        regions_dictionary[index] = regions_dictionary[index].split(',')
+        regions_dictionary[index] = regions_dictionary[index].split(",")
     regions_dictionary.pop()
 
     regions = []
@@ -67,24 +73,31 @@ def parse_regions(adress='Data\\RegionsInGADM.txt') :
     return regions, regions_dictionary
 
 
-blocked = ['г. Севастополь', 'Республика Крым', 'Московская область']
+blocked = ["г. Севастополь", "Республика Крым", "Московская область"]
 regions, regions_dictionary = parse_regions()
 data, mutations_names = parse_data()
 
-file_for_data = open("Data\\data.csv", 'w')
-file_for_regions = open("Data\\regions.txt", 'w')
-file_for_mutations_names = open("Data\\mutations_names.txt", 'w')
+file_for_data = open(Path("Data", "data.csv"), "w")
+file_for_regions = open(Path("Data", "regions.txt"), "w")
+file_for_mutations_names = open(Path("Data", "mutations_names.txt"), "w")
 print(data.to_csv(index=False), file=file_for_data)
 
 for region_index in range(len(regions)):
     if region_index != len(regions) - 1:
-        print(f"{regions[region_index]}", file=file_for_regions, end=',')
+        print(f"{regions[region_index]}", file=file_for_regions, end=",")
     else:
-        print(f"{regions[region_index]}", file=file_for_regions, end='')
+        print(f"{regions[region_index]}", file=file_for_regions, end="")
 
 for mutation_name_index in range(len(mutations_names)):
     if mutation_name_index != len(mutations_names) - 1:
-        print(f"{mutations_names[mutation_name_index]}", file=file_for_mutations_names, end=',')
+        print(
+            f"{mutations_names[mutation_name_index]}",
+            file=file_for_mutations_names,
+            end=",",
+        )
     else:
-        print(f"{mutations_names[mutation_name_index]}", file=file_for_mutations_names, end='')
-
+        print(
+            f"{mutations_names[mutation_name_index]}",
+            file=file_for_mutations_names,
+            end="",
+        )
