5f103c643c62deff6132a67db740992e223774fb
Mitdia <knorre.mitya@mail.ru>
2021-05-27 18:37:09 +0300

    Some debug code removed

--- Server/helpers/security.py
+++ Server/helpers/security.py
@@ -2,7 +2,6 @@ from datetime import datetime
 
 def check_date_format(date_string):
     format = "%Y-%m-%d"
-    print(datetime.strptime(date_string, format))
     try:
         datetime.strptime(date_string, format)
         return True
