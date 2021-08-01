4f4a8a68ca8a38bb2556040875a9422aba644716
Mitdia <knorre.mitya@mail.ru>
2021-08-02 02:56:13 +0300

    embed changed

--- flask/app/static/js/embed_script.js
+++ flask/app/static/js/embed_script.js
@@ -1,85 +1,160 @@
 var genes_array = [];
 var current_gene = "";
+var current_choice_section = "lineageSection";
 var url_string = window.location.href;
 var url = new URL(url_string);
 var max_date = url.searchParams.get("max_date");
 var min_date = url.searchParams.get("min_date");
 var language = url.searchParams.get("lang");
 var mutation = url.searchParams.get("mutation");
+var window_width = window.innerWidth;
 
-function add_gene_dropdown(gene, mutation_dropdown, gene_dropdown) {
-  var gene_button = document.createElement("div");
-  gene_button.className = "w3-bar-item w3-dropdown-click";
-  var gene_button_button = document.createElement("button");
-  gene_button_button.id = gene;
-  gene_button_button.textContent = gene;
-  gene_button_button.className = "w3-button";
-  gene_button_button.onclick = function() {geneDropdown(this.id)};
-  var gene_button_content = document.createElement("div");
-  gene_button_content.id = gene + ":";
-  gene_button_content.className = "w3-dropdown-content w3-bar-block w3-border";
-  gene_button_content.style = "max-height:250px; overflow:auto;"
-  gene_button.appendChild(gene_button_button);
-  mutation_dropdown.appendChild(gene_button_content);
-  gene_dropdown.appendChild(gene_button);
-  return gene_button_content
+
+async function embedBokehElement(address, id) {
+  let response = await fetch(`/${address}?mutation=${mutation}&lang=${language}&max_date=${max_date}&min_date=${min_date}&window_width=${window_width}`);
+  let item = await response.json();
+  Bokeh.embed.embed_item(item, id);
 }
 
 
-function init(mutations, max_date) {
-  max_date = max_date;
-  var mutationDropdownButton =  document.getElementById("mutationDropdownButton");
-  var geneDropdownButtonText = document.getElementById("geneDropdownButtonText");
-  var mutationDropdownButtonText =  document.getElementById("mutationDropdownButtonText");
-  var mutationNameDropdownButtonText =  document.getElementById("mutationNameDropdownButtonText");
-  var gene_dropdown = document.getElementById("gene");
-  var mutation_dropdown = document.getElementById("mutation");
+async function embedBokehElementWithLoader(address, id) {
+  var loader = document.getElementById(id + "Loader");
+  var content = document.getElementById(id);
+  content.style.display = "none";
+  loader.style.display = "block";
+  await embedBokehElement(address, id);
+  loader.style.display = "none";
+  content.style.display = "block";
+}
 
 
-  var mutation_array = mutation.split(':');
-  mutationDropdownButton.onclick = function () {geneDropdown(mutation.split(':')[0])};
-  geneDropdownButtonText.textContent = mutation_array[0];
-  mutationDropdownButtonText.textContent = mutation_array[0] + ':';
-  mutationNameDropdownButtonText.textContent = mutation_array[1];
+function add_gene_dropdown(gene, mutation_dropdown, gene_dropdown) {
+    var gene_button = document.createElement("div");
+    gene_button.className = "w3-bar-item w3-dropdown-click";
+    var gene_button_button = document.createElement("button");
+    gene_button_button.id = gene;
+    gene_button_button.textContent = gene;
+    gene_button_button.className = "w3-button";
+    gene_button_button.onclick = function() {
+        geneDropdown(this.id)
+    };
+    var gene_button_content = document.createElement("div");
+    gene_button_content.id = gene + ":";
+    gene_button_content.className = "w3-dropdown-content w3-bar-block w3-border";
+    gene_button_content.style = "max-height:250px; overflow:auto;"
+    gene_button.appendChild(gene_button_button);
+    mutation_dropdown.appendChild(gene_button_content);
+    gene_dropdown.appendChild(gene_button);
+    return gene_button_content
+}
 
-  mutation = mutations[0];
-  mutation_array = mutation.split(':');
-  var previous_gene = mutation_array[0];
-  gene_button_content = add_gene_dropdown(previous_gene, mutation_dropdown, gene_dropdown);
 
-  genes_array.push(previous_gene);
+function init_home_section() {
 
-  for (var i = 0; i < mutations.length; i++) {
-    mutation = mutations[i];
-    mutation_array = mutation.split(':');
-    var gene = mutation_array[0];
-    var mutation_name = mutation_array[1];
-    if (gene != previous_gene)
-    {
-        genes_array.push(gene);
-        var previous_gene = gene;
-        gene_button_content = add_gene_dropdown(gene, mutation_dropdown, gene_dropdown);
-    }
-    var mutation_button = document.createElement("a");
-    mutation_button.textContent = mutation_name;
-    mutation_button.className = "w3-bar-item w3-button";
-    mutation_button.id = mutation;
-    mutation_button.onclick = function () {changeupdate(this.id)};
-    gene_button_content.appendChild(mutation_button);
+  var certainMutationSection = document.getElementsByClassName("certainMutationSection");
+  var homeButton = document.getElementById("homeButton");
+
+  homeButton.onclick = "";
+  homeButton.className = homeButton.className.replace("w3-button w3-theme-d5 w3-hover-blue w3-col l11 m11 s11 w3-border-right", "w3-blue w3-col");
+  homeButton.style.textAlign = "center";
+  for (var i = 0;  i < certainMutationSection.length; i++) {
+    certainMutationSection[i].style.display = "none";
   }
 
-  var previous_dropdown = "None";
 
-  mutation = url.searchParams.get("mutation");
+  embedBokehElementWithLoader("map", "maincoronamap");
+  embedBokehElementWithLoader("plot", "coronaplot");
 
-  fetch(`/plot?mutation=${mutation}&lang=${language}&max_date=${max_date}&min_date=${min_date}`)
-      .then(function(response) { return response.json(); })
-      .then(function(item) { return Bokeh.embed.embed_item(item, "coronaplot"); })
+}
 
-  fetch(`/dateRangeSlider?mutation=${mutation}&lang=${language}&max_date=${max_date}&min_date=${min_date}`)
-      .then(function(response) { return response.json(); })
-      .then(function(item) { return Bokeh.embed.embed_item(item, "dateRangeSlider"); })
 
+function init_mutation_section() {
+  embedBokehElementWithLoader("map", "coronamap");
+
+  var homeSection = document.getElementsByClassName("homeSection");
+  for (var i = 0;  i < homeSection.length; i++) {
+    homeSection[i].style.display = "none";
+  }
+
+  var certainMutationsButton = document.getElementById("certainMutationsButton");
+  certainMutationsButton.onclick = "";
+  certainMutationsButton.className = certainMutationsButton.className.replace("w3-button w3-theme-d5 w3-hover-blue w3-col l11 m11 s11 w3-border-right", "w3-blue w3-col");
+  certainMutationsButton.style.textAlign = "center";
+}
+
+
+function init(mutations) {
+
+    var mutationDropdownButton = document.getElementById("mutationDropdownButton");
+    var geneDropdownButtonText = document.getElementById("geneDropdownButtonText");
+    var mutationDropdownButtonText = document.getElementById("mutationDropdownButtonText");
+    var mutationNameDropdownButtonText = document.getElementById("mutationNameDropdownButtonText");
+    var lineageDropdownButtonText = document.getElementById("lineageDropdownButtonText");
+    var lineage_dropdown = document.getElementById("lineage");
+    var gene_dropdown = document.getElementById("gene");
+    var mutation_dropdown = document.getElementById("mutation");
+
+    var mutation_array = mutation.split(':');
+    mutationDropdownButton.onclick = function() {
+        geneDropdown(mutation.split(':')[0])
+    };
+
+    mutation = url.searchParams.get("mutation");
+    embedBokehElement("dateRangeSlider", "dateRangeSlider");
+    if (mutation_array[0] == "ALL") {
+      init_home_section();
+    }
+    else if (mutation_array[0] == "lineage") {
+      init_mutation_section();
+      lineageDropdownButtonText.textContent = mutation_array[1];
+    }
+    else {
+      init_mutation_section();
+      switchToLineage();
+      geneDropdownButtonText.textContent = mutation_array[0];
+      mutationDropdownButtonText.textContent = mutation_array[0] + ':';
+      mutationNameDropdownButtonText.textContent = mutation_array[1];
+    }
+
+
+    mutation = mutations[0];
+    mutation_array = mutation.split(':');
+    var previous_gene = mutation_array[0];
+
+    for (var i = 0; i < mutations.length; i++) {
+        mutation = mutations[i];
+        mutation_array = mutation.split(':');
+        var gene = mutation_array[0];
+        var mutation_name = mutation_array[1];
+        if (gene != previous_gene) {
+            genes_array.push(gene);
+            var previous_gene = gene;
+            gene_button_content = add_gene_dropdown(gene, mutation_dropdown, gene_dropdown);
+        }
+        if (gene == "lineage") {
+
+            var mutation_button = document.createElement("a");
+            mutation_button.textContent = mutation_name;
+            mutation_button.className = "w3-bar-item w3-button";
+            mutation_button.id = mutation;
+            mutation_button.onclick = function() {
+                changeUpdateLineage(this.id)
+            };
+            lineage_dropdown.appendChild(mutation_button);
+        } else {
+            var mutation_button = document.createElement("a");
+            mutation_button.textContent = mutation_name;
+            mutation_button.className = "w3-bar-item w3-button";
+            mutation_button.id = mutation;
+            mutation_button.onclick = function() {
+                changeUpdate(this.id)
+            };
+            gene_button_content.appendChild(mutation_button);
+        }
+    }
+
+    var previous_dropdown = "None";
+    mutation = url.searchParams.get("mutation");
 }
 
 
